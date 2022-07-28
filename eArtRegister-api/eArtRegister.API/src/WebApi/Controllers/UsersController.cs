d2100982ebb421a4e9aed072021c83e51dfbbef3
Andrej Kalocanj Mohaci <andrej.kalocanj-mohaci@dunavnet.eu>
2022-07-28 16:24:26 +0200

    Deleted old endpoints

--- eArtRegister-api/eArtRegister.API/src/WebApi/Controllers/UsersController.cs
+++ eArtRegister-api/eArtRegister.API/src/WebApi/Controllers/UsersController.cs
@@ -22,53 +22,5 @@ namespace eArtRegister.API.WebApi.Controllers
                 return BadRequest(ex.Message);
             }
         }
-        //    [Route("api/[controller]")]
-        //    [ApiController]
-        //    public class UsersController : ApiControllerBase
-        //    {
-        //        [HttpPost]
-        //        [Route("search")]
-        //        [ApiExplorerSettings(GroupName = "v1")]
-        //        public async Task<ActionResult<List<UserDto>>> Search(SearchQuerry querry)
-        //        {
-        //            try
-        //            {
-        //                return await Mediator.Send(querry);
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                return BadRequest(ex.Message);
-        //            }
-        //        }
-
-        //        [HttpPost]
-        //        [Route("register")]
-        //        [ApiExplorerSettings(GroupName = "v1")]
-        //        public async Task<ActionResult<UserDto>> Register(RegisterCommand command)
-        //        {
-        //            try
-        //            {
-        //                return await Mediator.Send(command);
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                return BadRequest(ex.Message);
-        //            }
-        //        }
-
-        //        [HttpPost]
-        //        [Route("login")]
-        //        [ApiExplorerSettings(GroupName = "v1")]
-        //        public async Task<ActionResult<UserDto>> Login(LoginCommand command)
-        //        {
-        //            try
-        //            {
-        //                return await Mediator.Send(command);
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                return BadRequest(ex.Message);
-        //            }
-        //        }
     }
 }
