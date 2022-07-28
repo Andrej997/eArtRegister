47d9d707a8d5b03246ca600a6f6bd46a24d0e8ab
Andrej Kalocanj Mohaci <andrej.kalocanj-mohaci@dunavnet.eu>
2022-07-28 16:21:13 +0200

    Request a role

---
+++ eArtRegister-api/eArtRegister.API/src/Application/Users/Commands/RequestRolePermission/RequestRolePermissionCommand.cs
@@ -0,0 +1,57 @@
+﻿using eArtRegister.API.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eArtRegister.API.Application.Users.Commands.RequestRolePermission
+{
+    public class RequestRolePermissionCommand : IRequest
+    {
+        public long RoleId { get; set; }
+    }
+    public class RequestRolePermissionCommandHandler : IRequestHandler<RequestRolePermissionCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+
+        public RequestRolePermissionCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Unit> Handle(RequestRolePermissionCommand request, CancellationToken cancellationToken)
+        {
+            var user = _context.Users.Find(_currentUserService.UserId);
+            if (user == null)
+                throw new Exception("Unknown user");
+
+            if (request.RoleId < 1)
+                throw new Exception("Unknown role");
+
+            if (request.RoleId == (long)Domain.Enums.Role.Administrator)
+                throw new Exception("Only administrator can add you as administrator");
+            else if (request.RoleId == (long)Domain.Enums.Role.Buyer)
+            {
+                if (string.IsNullOrEmpty(user.MetamaskWallet))
+                    throw new Exception("You need to register a wallet to be a buyer");
+            }
+            else if (request.RoleId == (long)Domain.Enums.Role.Seller)
+            {
+                if (string.IsNullOrEmpty(user.MetamaskWallet))
+                    throw new Exception("You need to register a wallet to be a seller");
+            }
+
+            _context.UserRoles.Add(new Domain.Entities.UserRole
+            {
+                UserId = _currentUserService.UserId,
+                RoleId = request.RoleId
+            });
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
--- eArtRegister-api/eArtRegister.API/src/WebApi/Controllers/UsersController.cs
+++ eArtRegister-api/eArtRegister.API/src/WebApi/Controllers/UsersController.cs
@@ -1,60 +1,74 @@
-﻿//using eArtRegister.API.Application.Users.Commands.Login;
-//using eArtRegister.API.Application.Users.Commands.Register;
-//using eArtRegister.API.Application.Users.Queries.Search;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
+﻿using eArtRegister.API.Application.Users.Commands.RequestRolePermission;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
-//namespace eArtRegister.API.WebApi.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class UsersController : ApiControllerBase
-//    {
-//        [HttpPost]
-//        [Route("search")]
-//        [ApiExplorerSettings(GroupName = "v1")]
-//        public async Task<ActionResult<List<UserDto>>> Search(SearchQuerry querry)
-//        {
-//            try
-//            {
-//                return await Mediator.Send(querry);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
+namespace eArtRegister.API.WebApi.Controllers
+{
+    public class UsersController : ApiControllerBase
+    {
+        [HttpPost]
+        [Route("requestRolePermission")]
+        [ApiExplorerSettings(GroupName = "v1")]
+        public async Task<IActionResult> RequestRolePermission(RequestRolePermissionCommand command)
+        {
+            try
+            {
+                await Mediator.Send(command);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        //    [Route("api/[controller]")]
+        //    [ApiController]
+        //    public class UsersController : ApiControllerBase
+        //    {
+        //        [HttpPost]
+        //        [Route("search")]
+        //        [ApiExplorerSettings(GroupName = "v1")]
+        //        public async Task<ActionResult<List<UserDto>>> Search(SearchQuerry querry)
+        //        {
+        //            try
+        //            {
+        //                return await Mediator.Send(querry);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                return BadRequest(ex.Message);
+        //            }
+        //        }
 
-//        [HttpPost]
-//        [Route("register")]
-//        [ApiExplorerSettings(GroupName = "v1")]
-//        public async Task<ActionResult<UserDto>> Register(RegisterCommand command)
-//        {
-//            try
-//            {
-//                return await Mediator.Send(command);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
+        //        [HttpPost]
+        //        [Route("register")]
+        //        [ApiExplorerSettings(GroupName = "v1")]
+        //        public async Task<ActionResult<UserDto>> Register(RegisterCommand command)
+        //        {
+        //            try
+        //            {
+        //                return await Mediator.Send(command);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                return BadRequest(ex.Message);
+        //            }
+        //        }
 
-//        [HttpPost]
-//        [Route("login")]
-//        [ApiExplorerSettings(GroupName = "v1")]
-//        public async Task<ActionResult<UserDto>> Login(LoginCommand command)
-//        {
-//            try
-//            {
-//                return await Mediator.Send(command);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
-//    }
-//}
+        //        [HttpPost]
+        //        [Route("login")]
+        //        [ApiExplorerSettings(GroupName = "v1")]
+        //        public async Task<ActionResult<UserDto>> Login(LoginCommand command)
+        //        {
+        //            try
+        //            {
+        //                return await Mediator.Send(command);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                return BadRequest(ex.Message);
+        //            }
+        //        }
+    }
+}
